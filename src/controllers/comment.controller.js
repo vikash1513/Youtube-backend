ce93103e4c537b1b339d2a68d353e5cf0e79bc3a
vikash1513 <vikashsahu2215@gmail.com>
2024-08-07 18:02:30 +0530

    vid cont

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -238,7 +238,6 @@ const deleteComment = asyncHandler(async(req,res)=>{
     
 })
 
-
 export {
     getVideoComments,
     addComment,
--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -6,7 +6,6 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 
-
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
     if(!userId)
